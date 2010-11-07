d8cc7c1d0b92774adb1aef4721ed3973a384c29c
Atsushi Odagiri <aodagx@gmail.com>
2010-11-07 16:56:41 +0900

    trivial changes
    --HG--
    rename : README.txt => README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 here = os.path.dirname(__file__)
-readme = open(os.path.join(here, "README.txt")).read()
+readme = open(os.path.join(here, "README")).read()
 example = open(os.path.join(here, "rpc_example.txt")).read()
 changelog = open(os.path.join(here, "ChangeLog")).read()
 version="0.2.3"
