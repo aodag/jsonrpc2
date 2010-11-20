48d045dc1f7db5b0af9ce3184d7ad9fad5167b5a
Atsushi Odagiri <aodagx@gmail.com>
2010-11-20 16:44:10 +0900

    0.3 release
    --HG--
    branch : v_3

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ here = os.path.dirname(__file__)
 readme = open(os.path.join(here, "README")).read()
 example = open(os.path.join(here, "rpc_example.txt")).read()
 changelog = open(os.path.join(here, "ChangeLog")).read()
-version="0.3a2"
+version="0.3"
 
 tests_require = [
     "Nose",
