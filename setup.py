c3620e772e99b7282b628f302804553023d65e2a
Atsushi Odagiri <aodagx@gmail.com>
2014-04-06 21:11:07 +0900

    prep for 0.4.1
    --HG--
    branch : R_0_4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ here = os.path.dirname(__file__)
 readme = open(os.path.join(here, "README.rst")).read()
 example = open(os.path.join(here, "rpc_example.txt")).read()
 changelog = open(os.path.join(here, "ChangeLog")).read()
-version="0.4"
+version="0.4.1"
 
 tests_require = [
     "pytest",
